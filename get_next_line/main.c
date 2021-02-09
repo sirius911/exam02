91818ff1fde79266b54c670e8a90f4a40e6cc175
clorin <clorin@student.42.fr>
2021-02-09 17:09:31 +0100

    fix: main(void)

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -4,17 +4,15 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	char	*line = NULL;
 	int	result = 1;
-	int	fd = 0;
 	while(result != 0)
 	{
 		result = get_next_line(&line);
 		printf("%s\n", line);
 		free(line);
 	}
-	close(fd);
 	return (0);
 }
